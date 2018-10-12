297ba4454b1f62740c20e1c73fbc3d91ca5a1aff
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-10-12 10:06:52 +0900

    Add files via upload

---
+++ MicroCloud_Start.cpp
@@ -0,0 +1,18 @@
+/*
+ * MicroCloud_Start.cpp
+ *
+ *  Created on: Oct 12, 2018
+ *      Author: Tung1108
+ */
+
+#include <iostream>
+using namespace std;
+
+// int main() {
+int MicroCloudmain() {
+	cout << "Let's to explore Microsoft's unique techniques" << endl;
+	return 0;
+}
+
+
+
