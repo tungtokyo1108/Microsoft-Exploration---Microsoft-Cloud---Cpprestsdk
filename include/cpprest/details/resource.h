b940fd59374e4e7891aa4591dc910e7b54178c06
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-06 11:45:21 +0900

    Add files via upload

---
+++ include/cpprest/details/resource.h
@@ -0,0 +1,20 @@
+/*
+ * resource.h
+ *
+ *  Created on: Nov 6, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef INCLUDE_CPPREST_DETAILS_RESOURCE_H_
+#define INCLUDE_CPPREST_DETAILS_RESOURCE_H_
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#endif /* INCLUDE_CPPREST_DETAILS_RESOURCE_H_ */
