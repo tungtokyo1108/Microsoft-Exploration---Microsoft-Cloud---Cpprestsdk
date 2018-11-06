e000088069874613616ba081c4cd6c9f8328a627
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-06 11:54:44 +0900

    Add files via upload

---
+++ include/pplx/pplxinterface.h
@@ -0,0 +1,13 @@
+/*
+ * pplxinterface.h
+ *
+ *  Created on: Nov 6, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef INCLUDE_PPLX_PPLXINTERFACE_H_
+#define INCLUDE_PPLX_PPLXINTERFACE_H_
+
+
+
+#endif /* INCLUDE_PPLX_PPLXINTERFACE_H_ */
