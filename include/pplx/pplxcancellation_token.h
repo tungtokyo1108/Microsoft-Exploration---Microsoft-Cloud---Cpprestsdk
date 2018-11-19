e9ecf762c8be02f6f867ebfebd680f4aac764ecf
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-19 19:51:39 +0900

    Add files via upload

---
+++ include/pplx/pplxcancellation_token.h
@@ -0,0 +1,16 @@
+/*
+ * pplxcancellation_token.h
+ * https://github.com/PowerShell/cpprestsdk/blob/dev/Release/include/pplx/pplxcancellation_token.h
+ *
+ *  Created on: Nov 19, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
+
+#ifndef INCLUDE_PPLX_PPLXCANCELLATION_TOKEN_H_
+#define INCLUDE_PPLX_PPLXCANCELLATION_TOKEN_H_
+
+
+
+
+
+#endif /* INCLUDE_PPLX_PPLXCANCELLATION_TOKEN_H_ */
